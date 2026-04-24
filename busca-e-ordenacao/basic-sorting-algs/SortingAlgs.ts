53a22fda26296860608cafad76712670b1a5dba6
ederzin13 <edermurilo131313@gmail.com>
2026-04-24 00:19:46 -0300

    insertion sort...

--- busca-e-ordenacao/basic-sorting-algs/SortingAlgs.ts
+++ busca-e-ordenacao/basic-sorting-algs/SortingAlgs.ts
@@ -1,143 +1,143 @@
 export default class SortingAlgs {
-	private list: number[];
-	private readonly LEN: number = 0;
+  private list: number[];
+  private readonly LEN: number = 0;
 
-	public constructor(list: number[]) {
-		this.list = list;
-		this.LEN = this.getList().length;
-	}
+  public constructor(list: number[]) {
+    this.list = list;
+    this.LEN = this.getList().length;
+  }
 
-	public getList(): number[] {
-		return this.list;
-	}
+  public getList(): number[] {
+    return this.list;
+  }
 
-	public selectionSort(): number[] {
-		let unsorted: number[] = this.getList();
-		let sorted: number[] = [];
-		let menor: number = 0;
+  public selectionSort(): number[] {
+    let unsorted: number[] = this.getList();
+    let sorted: number[] = [];
+    let menor: number = 0;
 
-		while (unsorted.length > 0) {
-			for (let i: number = 0; i <= unsorted.length; i++) {
-				menor = this.getMenor(unsorted);
+    while (unsorted.length > 0) {
+      for (let i: number = 0; i <= unsorted.length; i++) {
+        menor = this.getMenor(unsorted);
 
-				if (unsorted[i] === menor) {
-					unsorted.splice(i, 1);
-					sorted.push(menor);
-				}
-			}
-		}
+        if (unsorted[i] === menor) {
+          unsorted.splice(i, 1);
+          sorted.push(menor);
+        }
+      }
+    }
 
-		return sorted;
-	}
+    return sorted;
+  }
 
-	//contemplem
-	public selectionSort2(): number[] {
-		let list: number[] = this.getList();
-		let sorted: number[] = [];
-		const LEN: number = list.length;
+  //contemplem
+  public selectionSort2(): number[] {
+    let list: number[] = this.getList();
+    let sorted: number[] = [];
+    const LEN: number = list.length;
 
-		while (sorted.length < LEN) {
-			let min: number = this.getMenor(list);
-			let toSplice: number = this.getPosition(list, min);
-			list.splice(toSplice, 1);
-			sorted.push(min);
-		}
+    while (sorted.length < LEN) {
+      let min: number = this.getMenor(list);
+      let toSplice: number = this.getPosition(list, min);
+      list.splice(toSplice, 1);
+      sorted.push(min);
+    }
 
-		return sorted;
-	}
+    return sorted;
+  }
 
-	//obra prima
-	public selectionSort3(): number[] {
-		let list: number[] = this.getList();
-		let j: number = 0;
+  //obra prima
+  public selectionSort3(): number[] {
+    let list: number[] = this.getList();
+    let j: number = 0;
 
-		while (!this.isSorted(list)) {
-			let min: number = list[j];
+    while (!this.isSorted(list)) {
+      let min: number = list[j];
 
-			for (let i: number = j; i <= list.length; i++) {
-				if (list[i] < min) {
-					min = list[i];
-					list[i] = list[j];
-					list[j] = min;
-				}
-			}
+      for (let i: number = j; i <= list.length; i++) {
+        if (list[i] < min) {
+          min = list[i];
+          list[i] = list[j];
+          list[j] = min;
+        }
+      }
 
-			j++;
-		}
+      j++;
+    }
 
-		return list;
-	}
+    return list;
+  }
 
-	public bubbleSort(): number[] {
-		let list: number[] = this.getList();
-		let j: number = list.length;
+  public bubbleSort(): number[] {
+    let list: number[] = this.getList();
+    let j: number = list.length;
 
-		while (j > 0) {
-			for (let i: number = 0; i < j; i++) {
-				if (list[i] > list[i + 1]) {
-					let aux: number = list[i];
-					list[i] = list[i + 1];
-					list[i + 1] = aux;
-				}
-			}
+    while (j > 0) {
+      for (let i: number = 0; i < j; i++) {
+        if (list[i] > list[i + 1]) {
+          let aux: number = list[i];
+          list[i] = list[i + 1];
+          list[i + 1] = aux;
+        }
+      }
 
-			j--;
-		}
+      j--;
+    }
 
-		return list;
-	}
+    return list;
+  }
 
-	// que podridão
-	public insertionSort(): number[] {
-		let unsorted: number[] = this.getList();
-		let sorted: number[] = this.genEmptyList(unsorted.length);
-		let current: number = 0;
+  // que podridão
+  public insertionSort(): number[] {
+    let unsorted: number[] = this.getList();
+    let sorted: number[] = this.genEmptyList(unsorted.length);
+    let current: number = 0;
 
-		//[10, 5, 9, 2, 8, 3, 7, 4, 6, 1]
+    //[10, 5, 9, 2, 8, 3, 7, 4, 6, 1]
 
-		//inserindo o primeiro elemento
-		sorted[0] = unsorted[0];
+    //inserindo o primeiro elemento
+    sorted[0] = unsorted[0];
 
-		for (let i: number = 0; i < this.LEN; i++) {}
+    for (let i: number = 0; i < this.LEN; i++) {}
 
-		return sorted;
-	}
+    return sorted;
+  }
 
-	private genEmptyList(len: number): number[] {
-		let list: number[] = [];
+  private genEmptyList(len: number): number[] {
+    let list: number[] = [];
 
-		for (let i: number = 0; i < len; i++) {
-			list.push(0);
-		}
+    for (let i: number = 0; i < len; i++) {
+      list.push(0);
+    }
 
-		return list;
-	}
+    return list;
+  }
 
-	private getMenor(list: number[]): number {
-		let menor: number = list[0];
+  private getMenor(list: number[]): number {
+    let menor: number = list[0];
 
-		for (let i: number = 0; i <= this.LEN; i++) {
-			if (list[i] < menor) menor = list[i];
-		}
+    for (let i: number = 0; i <= this.LEN; i++) {
+      if (list[i] < menor) menor = list[i];
+    }
 
-		return menor;
-	}
+    return menor;
+  }
 
-	private getPosition(list: number[], num: number): number {
-		let position: number = 0;
+  private getPosition(list: number[], num: number): number {
+    let position: number = 0;
 
-		for (let i: number = 0; i <= list.length; i++) {
-			if (list[i] === num) position = i;
-		}
+    for (let i: number = 0; i <= list.length; i++) {
+      if (list[i] === num) position = i;
+    }
 
-		return position;
-	}
+    return position;
+  }
 
-	public isSorted(list: number[]): boolean {
-		for (let i: number = 0; i <= list.length; i++) {
-			if (list[i] > list[i + 1]) return false;
-		}
+  public isSorted(list: number[]): boolean {
+    for (let i: number = 0; i <= list.length; i++) {
+      if (list[i] > list[i + 1]) return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 }
