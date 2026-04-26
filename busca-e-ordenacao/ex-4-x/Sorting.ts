5cf62c302fa1fb72254cd6d8afaaf75fac05243e
ederzin13 <edermurilo131313@gmail.com>
2026-04-25 23:57:25 -0300

    extremidade mais próxima

--- busca-e-ordenacao/ex-4-x/Sorting.ts
+++ busca-e-ordenacao/ex-4-x/Sorting.ts
@@ -1,6 +1,7 @@
 export default class Sorting {
 	private list: number[];
 	private readonly LEN: number;
+	private readonly RANGE: number = 20;
 
 	public constructor() {
 		this.list = this.genList();
@@ -19,7 +20,7 @@ export default class Sorting {
 		let list: number[] = [];
 
 		for (let i: number = 0; i < 20; i++) {
-			list.push(Math.ceil(Math.random() * 20));
+			list.push(Math.ceil(Math.random() * this.RANGE));
 		}
 
 		return list;
@@ -117,5 +118,50 @@ export default class Sorting {
 		}
 
 		return this.getMin(list, pos + 1, min);
+	}
+
+	//exercício 9
+	public findX(x: number): number {
+		if (this.mayBePresent(x)) {
+			let list: number[] = this.getList();
+
+			switch (this.closest(x)) {
+				case 0: {
+					console.log("Extremidade esquerda");
+
+					for (let i: number = 0; i <= this.LEN; i++) {
+						if (list[i] === x)
+							return i;
+					}
+				}
+
+				case 1: {
+					console.log("Extremidade direita");
+
+					for (let i: number = list[this.LEN - 1]; i >= 0; i--) {
+						if (list[i] === x)
+							return i;
+					}
+				}
+			}
+		}
+
+		return 0;
+	}
+
+	private mayBePresent(x: number): boolean {
+		let list: number[] = this.getList();
+
+		if (list[0] <= x && list[this.LEN - 1] >= x)
+			return true;
+
+		return false;
+	}
+
+	private closest(x: number): number {
+		if (x > Math.ceil(this.RANGE / 2))
+			return 1;
+
+		return 0;
 	}
 }
--- busca-e-ordenacao/ex-4-x/index.ts
+++ busca-e-ordenacao/ex-4-x/index.ts
@@ -1,9 +1,20 @@
 import Sorting from "./Sorting";
 
 let test: Sorting = new Sorting();
-// console.log(test.getList());
+console.log(test.getList());
 // console.log(test.betterBubble());
 // test.shuffle();
 // console.log(test.getList());
 // console.log(test.gnomeSort());
-console.log(test.recursiveSelection());
+// console.log(test.recursiveSelection());
+
+//exercício 9
+
+//chamando um método pra ordenar
+test.betterBubble();
+console.log(test.getList());
+
+//efetuando a busca
+console.log(test.findX(6));
+//se o número estiver presente, retorna a posição do mesmo no array
+//se não, retorna 0
