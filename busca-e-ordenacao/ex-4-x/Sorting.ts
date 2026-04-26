871c5e10ed476288f58e267adc69c5bbcddd5508
ederzin13 <edermurilo131313@gmail.com>
2026-04-25 23:17:04 -0300

    selection sort recursivo

--- busca-e-ordenacao/ex-4-x/Sorting.ts
+++ busca-e-ordenacao/ex-4-x/Sorting.ts
@@ -1,108 +1,121 @@
 export default class Sorting {
-  private list: number[];
-  private readonly LEN: number;
+	private list: number[];
+	private readonly LEN: number;
 
-  public constructor() {
-    this.list = this.genList();
-    this.LEN = this.list.length;
-  }
+	public constructor() {
+		this.list = this.genList();
+		this.LEN = this.list.length;
+	}
 
-  public getList(): number[] {
-    return this.list;
-  }
+	public getList(): number[] {
+		return this.list;
+	}
 
-  public setList(list: number[]) {
-    this.list = list;
-  }
+	public setList(list: number[]) {
+		this.list = list;
+	}
 
-  private genList(): number[] {
-    let list: number[] = [];
+	private genList(): number[] {
+		let list: number[] = [];
 
-    for (let i: number = 0; i < 20; i++) {
-      list.push(Math.ceil(Math.random() * 20));
-    }
+		for (let i: number = 0; i < 20; i++) {
+			list.push(Math.ceil(Math.random() * 20));
+		}
 
-    return list;
-  }
+		return list;
+	}
 
-  public isAscending(list: number[]): boolean {
-    let l: number[] = list;
+	public isAscending(list: number[]): boolean {
+		let l: number[] = list;
 
-    for (let i: number = 0; i <= this.LEN; i++) {
-      if (l[i] > l[i + 1]) return false;
-    }
+		for (let i: number = 0; i <= this.LEN; i++) {
+			if (l[i] > l[i + 1]) return false;
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  public betterBubble() {
-    let list: number[] = this.getList();
+	public betterBubble() {
+		let list: number[] = this.getList();
 
-    while (!this.isAscending(list)) {
-      for (let i: number = 0; i <= this.LEN; i++) {
-        if (list[i] > list[i + 1]) {
-          let aux: number = list[i];
-          list[i] = list[i + 1];
-          list[i + 1] = aux;
-        }
-      }
-    }
+		while (!this.isAscending(list)) {
+			for (let i: number = 0; i <= this.LEN; i++) {
+				if (list[i] > list[i + 1]) {
+					let aux: number = list[i];
+					list[i] = list[i + 1];
+					list[i + 1] = aux;
+				}
+			}
+		}
 
-    this.setList(list);
+		this.setList(list);
 
-    return list;
-  }
+		return list;
+	}
 
-  public shuffle(): void {
-    let list: number[] = this.getList();
-    let shuffled: number[] = [];
+	public shuffle(): void {
+		let list: number[] = this.getList();
+		let shuffled: number[] = [];
 
-    for (let i: number = 0; i < this.LEN; i++) {
-      let position: number = Math.ceil(Math.random() * list.length - 1);
+		for (let i: number = 0; i < this.LEN; i++) {
+			let position: number = Math.ceil(Math.random() * list.length - 1);
 
-      shuffled.push(list[position]);
-      list.splice(position, 1);
-    }
+			shuffled.push(list[position]);
+			list.splice(position, 1);
+		}
 
-    this.setList(shuffled);
-  }
+		this.setList(shuffled);
+	}
 
-  public gnomeSort(): number[] {
-    let list: number[] = this.getList();
+	public gnomeSort(): number[] {
+		let list: number[] = this.getList();
 
-    for (let i: number = 0; i < this.LEN; i++) {
-      if (i > 0) {
-        if (list[i] < list[i - 1]) {
-          let aux: number = list[i];
-          list[i] = list[i - 1];
-          list[i - 1] = aux;
-          i = i - 2;
-        }
-      }
-    }
+		for (let i: number = 0; i < this.LEN; i++) {
+			if (i > 0) {
+				if (list[i] < list[i - 1]) {
+					let aux: number = list[i];
+					list[i] = list[i - 1];
+					list[i - 1] = aux;
+					i = i - 2;
+				}
+			}
+		}
 
-    return list;
-  }
+		return list;
+	}
 
-  public recursiveSelection(pos: number = 0) {
-    let list: number[] = this.getList();
-    let min: number = this.getMin(list, pos);
+	public recursiveSelection(
+		pos: number = 0,
+		i: number = 0,
+		list: number[] = this.getList(),
+		min: number = this.getMin(list, pos),
+	): number[] 
+	
+	{
+		if (pos === list.length) 
+			return list;
 
-    let aux: number = list[pos];
-    list[pos] = min;
+		if (min === list[pos]) {
+			let aux: number = list[i];
+			list[i] = min;
+			list[pos] = aux;
 
-    return this.recursiveSelection(pos + 1);
-  }
+			return this.recursiveSelection(i + 1, i + 1, list);
+		}
 
-  private getMin(list: number[], pos: number, aux: number = list[pos]): number {
-    let min: number = aux;
+		return this.recursiveSelection(pos + 1, i, list);
+	}	
 
-    if (pos > list.length) return min;
+	private getMin(list: number[], pos: number, aux: number = list[pos]): number {
+		let min: number = aux;
 
-    if (list[pos] < min) {
-      min = list[pos];
-    }
+		if (pos > list.length) 
+			return min;
 
-    return this.getMin(list, pos + 1, min);
-  }
+		if (list[pos] < min) {
+			min = list[pos];
+		}
+
+		return this.getMin(list, pos + 1, min);
+	}
 }
--- busca-e-ordenacao/ex-4-x/index.ts
+++ busca-e-ordenacao/ex-4-x/index.ts
@@ -1,10 +1,9 @@
 import Sorting from "./Sorting";
 
 let test: Sorting = new Sorting();
-test.setList([6, 2, 9, 4, 7, 1, 8, 5, 0]);
 // console.log(test.getList());
 // console.log(test.betterBubble());
 // test.shuffle();
 // console.log(test.getList());
 // console.log(test.gnomeSort());
-console.log(test.getMin([6, 2, 9, 4, 7, 8, 5], 0));
+console.log(test.recursiveSelection());
